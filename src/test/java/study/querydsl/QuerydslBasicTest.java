b6555669770528cf4e0cd51da52bdf7196df213f
Kim Juho <sv05159@naver.com>
2021-12-02 09:55:54 +0900

    기본 Q-Type 활용
    Q클래스 인스턴스를 사용하는 2가지 방법
    - 별칭 직접 지정
    - 기본 인스턴스 사용

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -56,7 +56,8 @@ public class QuerydslBasicTest {
 
     @Test
     public void startQuerydsl() {
-        QMember m = new QMember("m");
+//        QMember m = new QMember("m");  //별칭 직접 지정
+        QMember m = QMember.member; //기본 인스턴스 사용
 
         Member findMember = queryFactory
                 .select(m)
