dfc3780c766983dbe6e490a5174b74326cfbf94b
Kim Juho <sv05159@naver.com>
2021-12-03 17:43:51 +0900

    조인 - on절
    2. 연관관계 없는 엔티티 외부 조인
    - 하이버네이트 5.1부터 on 을 사용해서 서로 관계가 없는 필드로 외부 조인하는 기능이 추가되었다. 물론 내부 조인도 가능하다.
    - 주의! 문법을 잘 봐야 한다. leftJoin() 부분에 일반 조인과 다르게 엔티티 하나만 들어간다.
      - 일반조인: leftJoin(member.team, team)
      - on조인: from(member).leftJoin(team).on(xxx)

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -268,9 +268,28 @@ public class QuerydslBasicTest {
         List<Tuple> result = queryFactory
                 .select(member, team)
                 .from(member)
-//                .join(member.team, team).on(team.name.eq("teamA"))
-                .join(member.team, team)
-                .where(team.name.eq("teamA"))
+                .leftJoin(member.team, team).on(team.name.eq("teamA"))
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+    /**
+     * 연관관계 없는 엔티티 외부 조인
+     * 회원의 이름이 팀 이름과 같은 대상을 외부 조인
+     */
+    @Test
+    public void join_on_no_relation() {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(team).on(member.username.eq(team.name))
                 .fetch();
 
         for (Tuple tuple : result) {
