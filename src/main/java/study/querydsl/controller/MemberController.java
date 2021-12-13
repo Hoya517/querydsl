67c26925db2c8f7800844014ffa4b5f32f0f0eff
Kim Juho <sv05159@naver.com>
2021-12-13 15:53:13 +0900

    Querydsl Web 지원
    - 공식 URL: https://docs.spring.io/spring-data/jpa/docs/2.2.3.RELEASE/reference/html/#core.web.type-safe
    
    한계점
    - 단순한 조건만 가능(eq, contains, like)
    - 조건을 커스텀하는 기능이 복잡하고 명시적이지 않음
    - 컨트롤러가 Querydsl에 의존
    - 복잡한 실무환경에서 사용하기에는 한계가 명확

--- src/main/java/study/querydsl/controller/MemberController.java
+++ src/main/java/study/querydsl/controller/MemberController.java
@@ -33,4 +33,6 @@ public class MemberController {
     public Page<MemberTeamDto> searchMemberV3(MemberSearchCondition condition, Pageable pageable) {
         return memberRepository.searchPageComplex(condition, pageable);
     }
+
+    // Querydsl Web 지원
 }
